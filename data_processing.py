1f664c9f632bd4814f6b8c223829590b14137d27
Alixyvtte <102790830+Alixyvtte@users.noreply.github.com>
2023-11-07 22:25:21 +0800

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -4,23 +4,36 @@ import pandas as pd
 import numpy as np
 
 def dataPreprocess(filename, input_ids, input_types, input_masks, label, urltype):
-    pad_size = 200  # 也称为 max_len (前期统计分析，文本长度最大值为38，取32即可覆盖99%)
-    bert_path = "/hy-tmp/charbert-bert-wiki/"
-    tokenizer = BertTokenizer(vocab_file=bert_path + "vocab.txt")  # 初始化分词器
+    """
+    Preprocess data from a file containing URLs and labels.
+
+    :param filename: The path to the data file.
+    :param input_ids: List to store input char IDs.
+    :param input_types: List to store segment IDs.
+    :param input_masks: List to store attention masks.
+    :param label: List to store labels.
+    :param urltype: The type of URL (0 for benign, 1 for malware).
+    :return: None
+    """
+    pad_size = 200
+    # Also known as max_len (Based on preliminary analysis,
+    # the maximum text length is 38, taking 32 to cover 99%)
+    bert_path = "charbert-bert-wiki/"
+    tokenizer = BertTokenizer(vocab_file=bert_path + "vocab.txt")  # Initialize the tokenizer
     with open(filename, encoding='utf-8') as f:
         for i, l in tqdm(enumerate(f)):
             x1 = l.strip()
             x1 = tokenizer.tokenize(x1)
             tokens = ["[CLS]"] + x1 + ["[SEP]"]
 
-            # 得到input_id, seg_id, att_mask
+            # Get input_id, seg_id, att_mask
             ids = tokenizer.convert_tokens_to_ids(tokens)
             types = [0] * (len(ids))
             masks = [1] * len(ids)
 
-            # 短则补齐，长则切断
+            # Pad if short, truncate if long
             if len(ids) < pad_size:
-                types = types + [1] * (pad_size - len(ids))  # mask部分 segment置为1
+                types = types + [1] * (pad_size - len(ids))  # Set segment to 1 for the masked part
                 masks = masks + [0] * (pad_size - len(ids))
                 ids = ids + [0] * (pad_size - len(ids))
             else:
@@ -39,24 +52,34 @@ def dataPreprocess(filename, input_ids, input_types, input_masks, label, urltype
 
 
 def dataPreprocessFromCSV(filename, input_ids, input_types, input_masks, label):
+    """
+    Preprocess data from a CSV file containing URLs and labels.
+
+    :param filename: The path to the CSV data file.
+    :param input_ids: List to store input char IDs.
+    :param input_types: List to store segment IDs.
+    :param input_masks: List to store attention masks.
+    :param label: List to store labels.
+    :return: None
+    """
     pad_size = 200
     bert_path = "/hy-tmp/charbert-bert-wiki/"
-    tokenizer = BertTokenizer(vocab_file=bert_path + "vocab.txt")  # 初始化分词器
+    tokenizer = BertTokenizer(vocab_file=bert_path + "vocab.txt")  # Initialize the tokenizer
 
     data = pd.read_csv(filename, encoding='utf-8')
     for i, row in tqdm(data.iterrows(), total=len(data)):
-        x1 = row['url']  # 请替换为你CSV文件中文本数据所在的列名
+        x1 = row['url']  # Replace with the column name in your CSV file where the text data is located
         x1 = tokenizer.tokenize(x1)
         tokens = ["[CLS]"] + x1 + ["[SEP]"]
 
-        # 得到input_id, seg_id, att_mask
+        # Get input_id, seg_id, att_mask
         ids = tokenizer.convert_tokens_to_ids(tokens)
         types = [0] * (len(ids))
         masks = [1] * len(ids)
 
-        # 短则补齐，长则切断
+        # Pad if short, truncate if long
         if len(ids) < pad_size:
-            types = types + [1] * (pad_size - len(ids))  # mask部分 segment置为1
+            types = types + [1] * (pad_size - len(ids))  # Set segment to 1 for the masked part
             masks = masks + [0] * (pad_size - len(ids))
             ids = ids + [0] * (pad_size - len(ids))
         else:
@@ -75,13 +98,22 @@ def dataPreprocessFromCSV(filename, input_ids, input_types, input_masks, label):
             label.append([0])
 
 def spiltDatast(input_ids, input_types, input_masks, label):
-    # 随机打乱索引
+    """
+    Split the dataset into training and testing sets.
+
+    :param input_ids: List of input character IDs.
+    :param input_types: List of segment IDs.
+    :param input_masks: List of attention masks.
+    :param label: List of labels.
+    :return: Split datasets for training and testing.
+    """
+    # Randomly shuffle the indices
     random_order = list(range(len(input_ids)))
-    np.random.seed(2020)  # 固定种子
+    np.random.seed(2020)  # Fix the seed
     np.random.shuffle(random_order)
     print(random_order[:10])
 
-    # 4:1 划分训练集和测试集
+    # Split the dataset into 80% training and 20% testing
     input_ids_train = np.array([input_ids[i] for i in random_order[:int(len(input_ids) * 0.8)]])
     input_types_train = np.array([input_types[i] for i in random_order[:int(len(input_ids) * 0.8)]])
     input_masks_train = np.array([input_masks[i] for i in random_order[:int(len(input_ids) * 0.8)]])
